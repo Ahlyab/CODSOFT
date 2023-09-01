58916eb7bc8646a8034219a536cbe8ed56092ba2
unknown <ahlyabasad@gmail.com>
2023-09-01 21:37:03 +0500

    mysql Jconnector added to project structure

---
+++ Java/Task 5 Course Enrollment System/src/DataBase/DatabaseConnection.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+public class DatabaseConnection {
+    public static Connection connection;
+
+    public static void connectDatabase() {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql:// localhost:3306/course_management","root","admin");
+//            Statement stmt = conn.createStatement();
+//            ResultSet rs = stmt.executeQuery("select * from students");
+//            while(rs.next()) {
+//                System.out.println(rs.getString(2));
+//            }
+
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+    }
+
+    public static  void closeConnection() {
+        connection.close();
+    }
+    public static void main(String args[]){
+
+    }
+}
