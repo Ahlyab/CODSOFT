969babb26817853e8c9f2c4c96b88d745cbfbc01
unknown <ahlyabasad@gmail.com>
2023-09-01 22:52:48 +0500

    Added custom button class with custom font

---
+++ Java/Task 5 Course Enrollment System/src/CustomButtons.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CustomButtons extends JButton {
+    public CustomButtons() {
+        super();
+    }
+
+    public CustomButtons(Icon icon) {
+        super(icon);
+        this.setCustomFont();
+
+    }
+
+    public CustomButtons(String text) {
+        super(text);
+        this.setCustomFont();
+    }
+
+    public CustomButtons(Action a) {
+        super(a);
+        this.setCustomFont();
+
+    }
+
+    public CustomButtons(String text, Icon icon) {
+        super(text, icon);
+        this.setCustomFont();
+
+    }
+
+    @Override
+    public void setFont(Font font) {
+        super.setFont(font);
+    }
+
+    private void setCustomFont() {
+        this.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+    }
+}
