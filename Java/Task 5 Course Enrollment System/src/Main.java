a037e4d666e58033f4be7aa89a7c6b7c2a2fe559
unknown <ahlyabasad@gmail.com>
2023-09-01 21:42:46 +0500

    Added todo list

--- Java/Task 5 Course Enrollment System/src/Main.java
+++ Java/Task 5 Course Enrollment System/src/Main.java
@@ -1,5 +1,14 @@
+/*
+* Todo:
+*  1. implement course registration gui windows
+*  2. implement available course listing gui
+*  3. implement gui to check how many students enrolled certain course and which student enrolled in which courses
+*  4. enrollment cancel gui
+* */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
+
