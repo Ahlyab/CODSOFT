574ccae53386a8559e5ddc6eda73c07131af6518
unknown <ahlyabasad@gmail.com>
2023-09-03 12:43:37 +0500

    Implemented basic UI for register course window

--- Java/Task 5 Course Enrollment System/src/GUI_Windows/RegisterCourse.java
+++ Java/Task 5 Course Enrollment System/src/GUI_Windows/RegisterCourse.java
@@ -1,16 +1,51 @@
 package GUI_Windows;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class RegisterCourse extends Window {
 
-    private JFrame frame = new JFrame();
+    private JFrame frame;
+    private JComboBox<String> name;
+    private JComboBox<String> course;
+    private JButton register;
+
+    public RegisterCourse() {
+        this.frame = new JFrame("Register course");
+        this.name = new JComboBox<String>();
+        this.course = new JComboBox<String>();
+        this.register = new JButton("Register");
+
+    }
 
     @Override
     public void setupUI() {
         frame.setLayout(null);
-        frame.setSize(700,700);
+        frame.setSize(600,700);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // set bounds
+        name.setBounds(50, 50, 200, 40);
+        course.setBounds(300, 50, 200, 40);
+        register.setBounds(250, 140, 100,40);
+
+        // add components
+        frame.add(name);
+        frame.add(course);
+        frame.add(register);
+
+        // config
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                register.requestFocus();
+            }
+        });
+
+        name.addItem("Select name");
+        course.addItem("Select course");
+
         frame.setVisible(true);
     }
 }
