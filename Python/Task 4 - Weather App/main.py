3fcc12d6452fa4153ff988e793a3085a53d9e792
unknown <ahlyabasad@gmail.com>
2023-09-05 01:30:15 +0500

    UI updated & connected with backend

---
+++ Python/Task 4 - Weather App/main.py
@@ -0,0 +1,25 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+from PyQt5.QtWidgets import QAction
+from PyQt5.uic import loadUiType
+from PyQt5.QtGui import *
+
+
+
+ui, _ = loadUiType("weather_app.ui")
+
+class MainApp(QMainWindow, ui):
+        def __init__(self, parent=None):
+            super(MainApp, self).__init__(parent)
+            QMainWindow.__init__(self)
+
+            self.setupUi(self)
+
+def main():
+    app = QApplication(sys.argv)
+    windows = MainApp()
+    windows.show()
+    app.exec_()
+if __name__ == "__main__":
+    main()
