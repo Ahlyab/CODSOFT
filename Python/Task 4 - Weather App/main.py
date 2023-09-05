2841bef220e0e7103bcb6bbc85251aa5999a2a76
unknown <ahlyabasad@gmail.com>
2023-09-05 18:27:12 +0500

    Optimized imports

--- Python/Task 4 - Weather App/main.py
+++ Python/Task 4 - Weather App/main.py
@@ -1,7 +1,6 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import sys
-from PyQt5.QtWidgets import QAction
 from PyQt5.uic import loadUiType
 from PyQt5.QtGui import *
 from weather import *
