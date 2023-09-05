2e4ae1872a9de6e64d2826f54198cf551879bb77
unknown <ahlyabasad@gmail.com>
2023-09-05 18:21:26 +0500

    Task completed

--- Python/Task 4 - Weather App/main.py
+++ Python/Task 4 - Weather App/main.py
@@ -10,8 +10,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
 ui, _ = loadUiType("weather_app.ui")
 
 class MainApp(QMainWindow, ui):
@@ -21,15 +19,32 @@ class MainApp(QMainWindow, ui):
             QMainWindow.__init__(self)
 
             self.setupUi(self)
+            self.findbtn.clicked.connect(self.buttonclicked)
 
         def buttonclicked(self):
             input = self.input.text()
             data = get_weather_data(self._api, input)
+            if(data['cod'] == '404'):
+                dlg = QMessageBox(self)
+                dlg.setWindowTitle("Error!")
+                dlg.setText("Please enter correct city name")
+                dlg.exec()
+                self.input.setText("")
+                return
+
             report = get_weather_report(data)
+            self.updateFields(report)
 
         def updateFields(self, report):
             # update fields according to report data
-            pass
+            self.temperature.setText(str(report['temp']))
+            self.city.setText(report['city'])
+            self.country.setText(report['country'])
+            self.weather.setText(report['weather'])
+            self.feelsLike.setText(str(report['feels']))
+            self.pressure.setText(str(report['pressure']))
+            self.humidity.setText(str(report['humidity']))
+
 
 def main():
     app = QApplication(sys.argv)
