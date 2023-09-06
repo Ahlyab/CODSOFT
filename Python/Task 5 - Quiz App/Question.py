e19c265872c328cd374553d8e2fd7f8e635234d7
unknown <ahlyabasad@gmail.com>
2023-09-06 19:53:04 +0500

    Implemented Question Class

---
+++ Python/Task 5 - Quiz App/Question.py
@@ -0,0 +1,26 @@
+# // python class to implement Question that has a statement, 4 options and index of correct answer
+
+class AnswerOutOfBoundException(Exception):
+    pass
+
+class Question:
+    def __init__(self, question : str, options : list, correctAnswerIndex : int ):
+        self.question = question
+        self.options = options
+        if(correctAnswerIndex < 0 or correctAnswerIndex >= len(options)):
+            raise AnswerOutOfBoundException
+        else:
+            self.correctAnswerIndex = correctAnswerIndex
+
+        def getCorrectAnswer(self) -> str:
+            return self.options[self.correctAnswerIndex]
+        
+        def getOptions(self) -> list[str]:
+            return self.options
+        
+        def getQuestionStatement(Self) -> str:
+            return self.question
+        
+        def getOptionsSize(self) -> int:
+            return options.__len__() 
+        
