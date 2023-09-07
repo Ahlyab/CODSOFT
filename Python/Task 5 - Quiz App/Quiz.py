b5d1902315feb1c8428e3570da455a2197242f2e
unknown <ahlyabasad@gmail.com>
2023-09-07 20:32:36 +0500

    implemented Quiz and feedback class

--- Python/Task 5 - Quiz App/Quiz.py
+++ Python/Task 5 - Quiz App/Quiz.py
@@ -14,5 +14,20 @@ class Quiz:
         q = self.questions.pop(0)
         return q
     
-    
-        
+
+class FeedBack:
+    score = 0
+    QA = None
+    def __init__(self) -> None:
+        pass
+
+    def checkAnswer(self, question : Question, answer : str):
+        if(question.getCorrectAnswer() == answer):
+            self.score = self.score + 1
+        self.QA[question.getStatement()] = question.getCorrectAnswer()
+
+    def generateReport(self) -> str:
+        report = f"your score is {self.score} \n"
+        for question, answer in self.QA.items:
+            report = report + f"Q: {question} \n A: {answer} \n"
+        return report
