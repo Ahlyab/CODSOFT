7ff5cb9c089a7bc6e6b3bc734f5389b6a7aa3b53
unknown <ahlyabasad@gmail.com>
2023-09-07 21:17:54 +0500

    Added dummy questions

--- Python/Task 5 - Quiz App/Quiz.py
+++ Python/Task 5 - Quiz App/Quiz.py
@@ -13,6 +13,26 @@ class Quiz:
     def getQuestion(self):
         q = self.questions.pop(0)
         return q
+    def addDummyQuestions(self):
+        self.addQuestion(Question("What is the output of 'print(3 * 4)'?", ["12", "7", "0", "34"], 0))
+
+        self.addQuestion(Question("Which data structure is typically used for implementing a LIFO behavior?", ["Queue", "Array", "Stack", "Linked List"], 2))
+
+        self.addQuestion(Question("In Python, what does the 'len()' function do?", ["Returns the largest element in a list", "Returns the number of items in an object", "Returns the sum of all elements in a list", "Returns the smallest element in a list"], 1))
+
+        self.addQuestion(Question("What does the acronym 'SQL' stand for?", ["Structured Query Language", "Standard Query Language", "Sequential Query Language", "Structured Question Language"], 0))
+
+        self.addQuestion(Question("Which programming language is known for its use in web development and is often referred to as 'the language of the web'?", ["Python", "Java", "Ruby", "JavaScript"], 3))
+
+        self.addQuestion(Question("What is the purpose of the 'try...except' block in Python?", ["To define a function", "To declare a variable", "To handle exceptions and errors", "To create a loop"], 2))
+
+        self.addQuestion(Question("What does the 'git' version control system primarily help with?", ["Database management", "Code execution", "Project collaboration", "Source code versioning"], 3))
+
+        self.addQuestion(Question("Which of the following is not a primitive data type in Python?", ["int", "float", "boolean", "string"], 2))
+
+        self.addQuestion(Question("What does the acronym 'API' stand for in software development?", ["Application Programming Interface", "Advanced Programming Interface", "Automated Program Integration", "Application Process Integration"], 0))
+
+        self.addQuestion(Question("In object-oriented programming, what is the term used to describe a blueprint for creating objects?", ["Plan", "Recipe", "Class", "Template"], 2))
     
 
 class FeedBack:
