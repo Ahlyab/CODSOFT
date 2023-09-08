1d2c4b763c40ed87bafb9b0d342519bb6a48557d
unknown <ahlyabasad@gmail.com>
2023-09-09 01:55:09 +0500

    Bugs located

--- Python/Task 5 - Quiz App/Quiz.py
+++ Python/Task 5 - Quiz App/Quiz.py
@@ -28,27 +28,27 @@ class Quiz:
 
         self.addQuestion(Question("What does the 'git' version control system primarily help with?", ["Database management", "Code execution", "Project collaboration", "Source code versioning"], 3))
 
-        self.addQuestion(Question("Which of the following is not a primitive data type in Python?", ["int", "float", "boolean", "string"], 2))
+        # self.addQuestion(Question("Which of the following is not a primitive data type in Python?", ["int", "float", "boolean", "string"], 2))
 
-        self.addQuestion(Question("What does the acronym 'API' stand for in software development?", ["Application Programming Interface", "Advanced Programming Interface", "Automated Program Integration", "Application Process Integration"], 0))
+        # self.addQuestion(Question("What does the acronym 'API' stand for in software development?", ["Application Programming Interface", "Advanced Programming Interface", "Automated Program Integration", "Application Process Integration"], 0))
 
-        self.addQuestion(Question("In object-oriented programming, what is the term used to describe a blueprint for creating objects?", ["Plan", "Recipe", "Class", "Template"], 2))
+        # self.addQuestion(Question("In object-oriented programming, what is the term used to describe a blueprint for creating objects?", ["Plan", "Recipe", "Class", "Template"], 2))
     
 
 class FeedBack:
     score = 0
-    QA = None
+    QA = dict()
     def __init__(self) -> None:
         pass
 
     def checkAnswer(self, question : Question, answer : str):
         if(question.getCorrectAnswer() == answer):
             self.score = self.score + 1
-        self.QA[question.getStatement()] = question.getCorrectAnswer()
+        self.QA[question.getQuestionStatement()] = question.getCorrectAnswer()
 
     def generateReport(self) -> str:
         report = f"your score is {self.score} \n"
-        for question, answer in self.QA.items:
+        for question, answer in self.QA.items():
             report = report + f"Q: {question} \n A: {answer} \n"
         return report
 
--- Python/Task 5 - Quiz App/main.py
+++ Python/Task 5 - Quiz App/main.py
@@ -12,21 +12,65 @@ ui, _ = loadUiType("quiz_app.ui")
 class MainApp(QMainWindow, ui):
         quiz = Quiz()
         feedback = FeedBack()
+        current_question = None
         def __init__(self, parent=None):
             super(MainApp, self).__init__(parent)
             QMainWindow.__init__(self)
             self.setupUi(self)
             self.quiz.addDummyQuestions()
             self.addQuestion()
+            self.submit.clicked.connect(self.btnClicked)
 
         def addQuestion(self):
-            q = self.quiz.getQuestion()
-            self.question.setText(q.getQuestionStatement())
-            options = q.getOptions()
+            self.option1.setChecked(False)
+            # self.option1.setAutoExclusive(False)
+
+            self.option2.setChecked(False)
+            # self.option2.setAutoExclusive(False)
+
+            self.option3.setChecked(False)
+            # self.option3.setAutoExclusive(False)
+
+            self.option4.setChecked(False)
+            # self.option4.setAutoExclusive(False)
+
+
+            self.current_question = self.quiz.getQuestion()
+            self.question.setText(self.current_question.getQuestionStatement())
+            options = self.current_question.getOptions()
             self.option1.setText(options[0])
             self.option2.setText(options[1])
             self.option3.setText(options[2])
             self.option4.setText(options[3])
+
+        def btnClicked(self):
+            ans = ""
+            if(self.option1.isChecked):
+                 ans = self.option1.text()
+            elif (self.option2.isChecked):
+                 ans = self.option2.text()
+            elif (self.option3.isChecked):
+                 ans = self.option3.text()
+            elif (self.option4.isChecked):
+                 ans = self.option4.text()
+            elif (ans == ""):
+                dlg = QMessageBox(self)
+                dlg.setWindowTitle("Error!")
+                dlg.setText("Please at one option")
+                dlg.exec()
+                return
+            print(ans)
+            self.feedback.checkAnswer(self.current_question, ans )
+            ans = ""
+
+            if(self.quiz.questions.__len__() == 0):
+                dlg = QMessageBox(self)
+                dlg.setWindowTitle("Feedback")
+                dlg.setText(self.feedback.generateReport())
+                dlg.exec()
+                return
+
+            self.addQuestion()
             
 
 
