824c1af937286e13861fe0fc3dae8e3f1a2f1b1f
unknown <ahlyabasad@gmail.com>
2023-09-08 23:31:24 +0500

    main updated

--- Python/Task 5 - Quiz App/main.py
+++ Python/Task 5 - Quiz App/main.py
@@ -10,11 +10,24 @@ from Quiz import *
 ui, _ = loadUiType("quiz_app.ui")
 
 class MainApp(QMainWindow, ui):
+        quiz = Quiz()
+        feedback = FeedBack()
         def __init__(self, parent=None):
             super(MainApp, self).__init__(parent)
             QMainWindow.__init__(self)
-
             self.setupUi(self)
+            self.quiz.addDummyQuestions()
+            self.addQuestion()
+
+        def addQuestion(self):
+            q = self.quiz.getQuestion()
+            self.question.setText(q.getQuestionStatement())
+            options = q.getOptions()
+            self.option1.setText(options[0])
+            self.option2.setText(options[1])
+            self.option3.setText(options[2])
+            self.option4.setText(options[3])
+            
 
 
 def main():
